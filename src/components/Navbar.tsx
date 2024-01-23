9b7810b4bf77ab880b479c02559e8027b6f11fea
vitooputraa <vito.putra@ui.ac.id>
2024-01-23 22:39:00 +0700

    done

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,20 @@
+/* eslint-disable react/jsx-key */
+import Link from 'next/link'
+import { BsGithub } from 'react-icons/bs'
+
+const Navbar = () => {
+  return (
+    <header className="fixed inset-x-0 top-0 z-50 h-14 w-full bg-primary px-3 shadow-md shadow-secondary/20">
+      <div className="mx-auto flex h-full w-full max-w-7xl flex-row items-center justify-between">
+        <Link href="/">
+          <h1 className="text-2xl font-bold text-accent">Pokemon</h1>
+        </Link>
+        <Link href="https://github.com/vitooputraa">
+          <BsGithub className="text-3xl duration-500 ease-in-out hover:text-accent" />
+        </Link>
+      </div>
+    </header>
+  )
+}
+
+export default Navbar
---
+++ src/pages/index.tsx
@@ -0,0 +1,67 @@
+import PokemonCard from '@/components/PokemonCard'
+import { useInfiniteQuery } from '@/hooks'
+import type { NextPage } from 'next'
+import Head from 'next/head'
+import { useMemo } from 'react'
+import { useInView } from 'react-cool-inview'
+export interface Result {
+  name: string
+  url: string
+}
+export interface PokemonIDList {
+  count: number
+  next: string
+  previous?: any
+  results: Result[]
+}
+
+const Home: NextPage = () => {
+  const { data, next } = useInfiniteQuery()
+
+  const pokemons: any = useMemo(
+    () => data?.flatMap((page: any) => page?.results) ?? [],
+    [data]
+  )
+
+  const { observe } = useInView({
+    // For better UX, we can grow the root margin so the data will be loaded earlier
+    rootMargin: '300px',
+    // When the last item comes to the viewport
+    onEnter: ({ unobserve }) => {
+      // Pause observe when loading data
+      unobserve()
+
+      // Load more data
+      next()
+    },
+  })
+
+  return (
+    <>
+      <Head>
+        <title>Pokemon App - by Vito</title>
+        <meta name="description" content="Get information about all Pokemons" />
+        <meta property="og:title" content="Pokedéx" key="Pokedéx" />
+      </Head>
+
+      <ul className="mx-auto grid min-h-screen w-full max-w-7xl grid-cols-1 gap-4 p-10 pt-8 md:grid-cols-2 md:gap-5 lg:gap-7 lg:p-16 lg:pt-4 xl:w-2/3 2xl:grid-cols-3">
+        {pokemons?.map((data: any, index: number) => {
+          const isLast = index === pokemons.length - 1
+          const { name, url } = data
+
+          return (
+            <li
+              key={name}
+              ref={isLast ? observe : null}
+              className="h-80 w-full"
+            >
+              <PokemonCard url={url} index={++index} />
+            </li>
+          )
+        })}
+      </ul>
+    </>
+  )
+}
+
+export default Home
